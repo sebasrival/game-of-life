685852a3db55e964328993398fb77f69ee88be25
Sebastian Rivaldi <sebasrival@gmail.com>
2022-05-04 13:14:02 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -6,23 +6,39 @@ import Cells from './components/Cells';
 
 function App() {
 
-    const [col, setCol] = useState(100)
-    const [row, setRow] = useState(50)
+    const [col, setCol] = useState(70)
+    const [row, setRow] = useState(30)
     const [board, setBoard] = useBoard(row, col)
+    const [gameState, setGameState] = useState(false)
     const tam = col * 14
 
+    const start = () => {
+        setGameState(true)
+        console.log(gameState)
+    }
+
+    const pause = () => {
+        setGameState(false)
+        console.log(gameState)
+    }
 
     return (
-        <div>
-            <div className='title'>
+        <main>
+            <header className='title'>
                 <p>Game of Life!</p>
-            </div>
-            <div className='content'>
-                <div className='board' style={{width: tam + 'px'}}>
-                    <Cells board={board} setBoard={setBoard} row_t={row} col_t={col}></Cells>
+            </header>
+            <div className='content-center'>
+                <div className='content'>
+                    <nav className='button-box'>
+                        <button className='button' onClick={start}>Start</button>
+                        <button className='button' onClick={pause}>Pause</button>
+                    </nav>
+                    <div className='board' style={{width: tam + 'px'}}>
+                        <Cells board={board} setBoard={setBoard} row_t={row} col_t={col}></Cells>
+                    </div>
                 </div>
             </div>
-        </div>
+        </main>
         
     )
 }
